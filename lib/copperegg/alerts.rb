f1913e4645a56d7f0ceb48adc78b6938b75f755e
ppp0 <philipp.gysin@cargomedia.ch>
2014-09-15 18:46:28 +0200

    Simplify alert schedules creation a bit

--- lib/copperegg/alerts.rb
+++ lib/copperegg/alerts.rb
@@ -1,3 +1,5 @@
+require 'deep_merge'
+
 module Copperegg
   class Alerts
     attr_reader :schedules
@@ -18,25 +20,28 @@ module Copperegg
     end
 
     def modify_schedule(name, *args)
-      body = {:body => {}}
-      args.each { |arg| body[:body].merge!(arg) }
       @schedules.select { |h| h['name'] == name }.each do |schedule|
         @client.put('alerts/schedules/' + schedule['id'] + '.json', body)
       end
     end
 
     def create_schedule(name, *args)
-      body = {
-        'state' => 'enabled',
-        'duration' => 10,
-        'start_time' => Time.now.gmtime.strftime('%Y-%m-%dt%H:%M:%Sz'),
+      defaults = {
+          'name' => name,
+          'state' => 'enabled',
+          'duration' => 10,
+          'start_time' => Time.now.gmtime.strftime('%Y-%m-%dt%H:%M:%Sz'),
       }
-      args.each { |arg| body.merge!(arg) }
-      @client.post('alerts/schedules.json', body)
+      args.each { |arg| defaults.deep_merge!(arg) }
+      if result = @client.post?('alerts/schedules.json', defaults)
+        @schedules << result.parsed_response
+      else
+        warn("No alert schedule set (HTTP response code: #{result.code})")
+      end
     end
 
     def reset_schedules(name)
-      selected_schedules = @schedules.select! { |h| h['name'] == name }
+      selected_schedules = @schedules.reject! { |h| h['name'] == name }
       selected_schedules.each { |s| @client.delete("alerts/schedules/#{s['id']}.json") } if selected_schedules
     end
   end
