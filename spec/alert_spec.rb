b6e298f22443f5a7845b3d629f894846e6688305
ppp0 <philipp.gysin@cargomedia.ch>
2014-09-15 18:47:13 +0200

    Adapt tests for schedule handling

--- spec/alert_spec.rb
+++ spec/alert_spec.rb
@@ -30,23 +30,27 @@ describe Copperegg::Alerts do
 
   describe '#new' do
     it 'requests a list of all schedules' do
-      expect(@alerts.schedules) === Hash
+      expect(@alerts.schedules.size > 0).to be(true)
     end
   end
 
   describe 'create schedule' do
     it 'creates three new schedules with name "spec_test"' do
+      alerts_schedules_list_size_before = @alerts.schedules.size
       VCR.use_cassette('schedule_create', :record => :once, :match_requests_on => [:body_as_json], :allow_playback_repeats => true) do
         3.times { @alerts.create_schedule('spec_test', 'match' => {'tag' => ['foo']}, 'state' => 'disabled', 'duration' => 7, 'start_time' => '2014-09-14T10:21:40Z') }
       end
+      expect(@alerts.schedules.size - alerts_schedules_list_size_before).to eq(3)
     end
   end
 
   describe 'reset_schedules("spec_test")' do
     it 'removes all schedules with name "spec_test" from the list if any' do
+      alerts_schedules_list_size_before = @alerts.schedules.size
       VCR.use_cassette('schedule_reset', :record => :once, :match_requests_on => [:method, :path]) do
         @alerts.reset_schedules('spec_test')
       end
+      expect(alerts_schedules_list_size_before - @alerts.schedules.size).to eq(3)
     end
   end
 
